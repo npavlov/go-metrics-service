0a280e3be7713466c422c1e190a6159145d284bb
Nikita Pavlov <pavlovnp@outlook.com>
2024-10-31 14:20:23 +0100

    fix: use old way to send metrics

--- internal/agent/config/builder.go
+++ internal/agent/config/builder.go
@@ -13,7 +13,7 @@ type Config struct {
 	Address           string `env:"ADDRESS"         envDefault:"localhost:8080"`
 	ReportInterval    int64  `env:"REPORT_INTERVAL" envDefault:"10"`
 	PollInterval      int64  `env:"POLL_INTERVAL"   envDefault:"2"`
-	UseBatch          bool   `env:"USE_BATCH"       envDefault:"true"`
+	UseBatch          bool   `env:"USE_BATCH"       envDefault:"false"`
 	ReportIntervalDur time.Duration
 	PollIntervalDur   time.Duration
 }
